4d61b32f26a809d0323300423bbd074f72344cce
faris <farissoehartono@gmail.com>
2025-02-25 15:40:34 +0700

    adding praktikum 1

---
+++ Praktikum1/coba7.php
@@ -0,0 +1,13 @@
+<html>
+
+<body>
+  <?php
+  $val1 = "Aku ingin belajar ";
+  $val2 = "PHP dengan cepat ";
+  echo "Menggabungkan 2 string val 1 dengan val 2 : ";
+  echo $val1 . $val2;
+  echo "<br>";
+  ?>
+</body>
+
+</html>
